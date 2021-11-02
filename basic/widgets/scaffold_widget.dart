08e73668a592817a759d92e72af2d1103a89682c
Mohamad Zaki Mustafa <mohamadzakimustafa@gmail.com>
2021-11-02 10:44:58 +0800

    Update scaffold_widget.dart

--- basic/widgets/scaffold_widget.dart
+++ basic/widgets/scaffold_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(ScaffoldWidget());
+void main() => runApp(MyApp());
 
-class ScaffoldWidget extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
